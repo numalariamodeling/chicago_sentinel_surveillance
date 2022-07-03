f905c59aaca1d0bd76703afd78c5fc20c2d9730c
Reese Richardson <richardsonr43@gmail.com>
2022-07-03 18:01:06 -0500

    continuous comparison

--- helpers.py
+++ helpers.py
@@ -26,6 +26,10 @@ def percent_agreement(df1,
     new_df = new_df[(new_df['key_0'] >= min_date) & (new_df['key_0'] <= max_date)]
     return np.sum((new_df[param + '_x'] >= 1) == (new_df[param + '_y'] >= 1))/len(new_df)
 
+def crossings(df_1):
+    truth_series = (df_1['Q0.5'] >= 1.0).astype(int).diff()
+    return truth_series[truth_series != 0].dropna().index.values
+
 def spearmanr(df1, 
              df2, 
              param='Q0.5', 
@@ -36,7 +40,7 @@ def spearmanr(df1,
     '''
     new_df = pd.merge(df1, df2, left_on=df1.index, right_on=df2.index, how='inner')
     new_df = new_df[(new_df['key_0'] >= min_date) & (new_df['key_0'] <= max_date)]
-    return scipy.stats.spearmanr(new_df[param + '_x'], new_df[param + '_y']), len(new_df)
+    return scipy.stats.spearmanr(new_df[param + '_x'], new_df[param + '_y'])[0]
 
 def med_and_iqr(discrete_distrb):
     '''
@@ -60,3 +64,12 @@ def zip_in_zips(zip_code, zips):
     except:
         return_val = False
     return return_val
+
+def spearman_ci(array1, array2, alpha=0.05, n_boot=100):
+    percentiles = np.array([alpha/2, 1-(alpha/2)])
+    nom_val, nom_p = scipy.stats.spearmanr(array1, array2)
+    rho_array = []
+    for ii in range(n_boot):
+        indeces = np.random.choice(range(len(array1)), replace=True, size=len(array1))
+        rho_array.append(scipy.stats.spearmanr(array1[indeces], array2[indeces])[0])
+    return nom_val, nom_p, np.percentile(rho_array, 100*percentiles[0]), np.percentile(rho_array, 100*percentiles[1])
